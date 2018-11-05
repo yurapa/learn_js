f9af75ce92d1fe4f8d2f3c1dc6839d4fe6fa97a5
YuraPa <yura.paray@gmail.com>
2018-11-05 10:34:38 +0200

    updated CSS generator

--- practice/3_css_border-radius_generator/app.js
+++ practice/3_css_border-radius_generator/app.js
@@ -1,4 +1,10 @@
-function changeValue() {
+document.getElementById('rtl').oninput = cssGenerator;
+document.getElementById('rtr').oninput = cssGenerator;
+document.getElementById('rbr').oninput = cssGenerator;
+document.getElementById('rbl').oninput = cssGenerator;
+
+
+function cssGenerator() {
     var block = document.getElementById('wrapper'),
         rtl = document.getElementById('rtl').value,
         ttl = document.getElementById('ttl'),
@@ -7,12 +13,15 @@ function changeValue() {
         rbr = document.getElementById('rbr').value,
         tbr = document.getElementById('tbr'),
         rbl = document.getElementById('rbl').value,
-        tbl = document.getElementById('tbl');
+        tbl = document.getElementById('tbl'),
+        out = document.getElementById('out');
 
-        ttl.value = rtl;
-        ttr.value = rtr;
-        tbr.value = rbr;
-        tbl.value = rbl;
+    ttl.value = rtl;
+    ttr.value = rtr;
+    tbr.value = rbr;
+    tbl.value = rbl;
 
-        block.style.borderRadius = rtl + 'px ' + rtr + 'px ' + rbr + 'px ' + rbl + 'px';
+    block.style.borderRadius = rtl + 'px ' + rtr + 'px ' + rbr + 'px ' + rbl + 'px';
+    out.innerHTML = '-webkit-border-radius: ' + rtl + 'px ' + rtr + 'px ' + rbr + 'px ' + rbl + 'px; \n'
+    out.innerHTML += 'border-radius: ' + rtl + 'px ' + rtr + 'px ' + rbr + 'px ' + rbl + 'px;'
 }
